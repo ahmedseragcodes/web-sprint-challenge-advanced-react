fd36741a1023d7b29672e0704cc8dfcf2bcb6e80
ahmedseragcodes <ahmedserag1@gmail.com>
2021-02-12 10:01:28 -0500

    setup files / dependencies, created axios call and successfully rendering plants

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,6 +2,25 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
+  constructor(){
+    super();
+    this.state={
+      plants: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get("http://localhost:3333/plants")
+    .then((res)=>{
+      console.log("PLANTS ARRAY", res.data.plantsData);
+      this.setState({
+        plants: res.data.plantsData,
+      })
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
   // add state with a property called "plants" - initialize as an empty array
 
   // when the component mounts:
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,6 @@
 // write your custom hook here to control your checkout form
+import CheckoutForm from "../components/CheckoutForm";
+
+const useForm=()=>{
+
+}
