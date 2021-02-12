5e738ade056ea2d3640b645a76ea76a3ec08c8ba
ahmedseragcodes <ahmedserag1@gmail.com>
2021-02-12 10:37:33 -0500

    built custom hook that checks local storage for saved values for form first

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,6 +1,25 @@
 // write your custom hook here to control your checkout form
-import CheckoutForm from "../components/CheckoutForm";
+import React, { useState } from "react";
 
-const useForm=()=>{
+const useForm=(key, initialValue)=>{
+    
+    const [storedValue, setStoredValue]=useState(()=>{
+        
+        const item=JSON.parse(window.localStorage.getItem(key));
+
+        return item ? item : initialValue;
+
+    })
+
+  const setData=(value)=>{
+      setStoredValue(value);
+      window.localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  return [storedValue, setData];
+
 
 }
+
+
+export default useForm;
